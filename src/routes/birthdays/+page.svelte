212eb1de45d48baf30d51ab47c65bb9f1468f742
Benoît Hubert <benoithubert@gmail.com>
2026-03-10 16:59:47 +0100

    WIP chap. 3 - Make /birthdays page tests pass with 2 harcoded Birthday's

---
+++ src/routes/birthdays/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Birthday from "./Birthday.svelte";
+</script>
+
+<Birthday name="Hercules" dob="4/1/-2000" />
+<Birthday name="Athena" dob="7/14/-2002" />
---
+++ tests/birthdays.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@playwright/test';
+test('lists all birthdays', async ({ page }) => {
+	await page.goto('/birthdays');
+	await expect(page.getByText('Hercules')).toBeVisible();
+	await expect(page.getByText('Athena')).toBeVisible();
+});
